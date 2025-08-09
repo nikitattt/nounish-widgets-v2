957031e01f35e827d396f150d2fe277733145ee3
NG <0xng@proton.me>
2025-08-09 16:29:59 +0300

    chore: Replace @nouns/assets with @noundry/nouns-assets dependency in package.json and package-lock.json

--- backend/source/controllers/nouns.ts
+++ backend/source/controllers/nouns.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import axios, { AxiosResponse } from 'axios'
 import { ethers } from 'ethers'
-import { ImageData, getNounData } from '@nouns/assets'
+import { ImageData, getNounData } from '@noundry/nouns-assets'
 import { buildSVG } from '@nouns/sdk'
 import { shortAddress, shortENS } from '../utils/addressAndENSDisplayUtils'
 import { AlchemyProvider } from '@ethersproject/providers'
